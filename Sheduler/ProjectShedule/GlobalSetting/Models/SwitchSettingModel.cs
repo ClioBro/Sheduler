ceb8771ee64385f214ce07eee169524487818d1e
Insaf <89502937+ClioBro@users.noreply.github.com>
2022-02-28 17:43:28 +0300

    Create SwitchSettingModel.cs

---
+++ Sheduler/ProjectShedule/GlobalSetting/Models/SwitchSettingModel.cs
@@ -0,0 +1,12 @@
+﻿using ProjectShedule.Language.Resources.Pages.Setting;
+
+namespace ProjectShedule.GlobalSetting.Models
+{
+    public class SwitchSettingModel : BooleanValueElementCell
+    {
+        public SwitchSettingModel() : base(SettingResources.OnLabel, SettingResources.OffLabel)
+        {
+
+        }
+    }
+}
