4239620544a0acd4a1c3bfdb9555708010355313
Insaf <89502937+ClioBro@users.noreply.github.com>
2022-02-28 17:42:42 +0300

    Create IAnimate.cs

---
+++ Sheduler/ProjectShedule/Other/IAnimate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjectShedule.Other
+{
+    public interface IAnimate
+    {
+        bool IsAnimated { get; }
+        public Task SinInElementAsync(VisualElement animatebleObj, Action finish);
+    }
+}
