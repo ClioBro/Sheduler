a96c387ae0a03a329efa734a04c13a6662f75d52
Insaf <89502937+ClioBro@users.noreply.github.com>
2022-05-22 17:57:58 +0300

    Update MultipleSelectionDayEngine.cs

--- Sheduler/ProjectShedule/Shedule/DateCalendar/Controls/SelectionEngine/MultipleSelectionDayEngine.cs
+++ Sheduler/ProjectShedule/Shedule/DateCalendar/Controls/SelectionEngine/MultipleSelectionDayEngine.cs
@@ -1,14 +1,14 @@
 ﻿using ProjectShedule.Shedule.Calendar.Models;
 using ProjectShedule.Shedule.DateCalendar.Controls.SelectionEngine;
 using System;
-using System.Collections.ObjectModel;
+using Xamarin.CommunityToolkit.ObjectModel;
 
 namespace ProjectShedule.Shedule.Calendar.Controls.SelectionEngine
 {
     public abstract class BaseSelectionHasDateEngine<T> : ISelectionEngine<T> where T : IHasDate
     {
-        protected ObservableCollection<DateTime> _selectedItems = new ObservableCollection<DateTime>();
-        public ObservableCollection<DateTime> SelectedDatesTime { get => _selectedItems; set => _selectedItems = value; }
+        protected ObservableRangeCollection<DateTime> _selectedItems = new ObservableRangeCollection<DateTime>();
+        public ObservableRangeCollection<DateTime> SelectedDatesTime { get => _selectedItems; set => _selectedItems = value; }
         public abstract void SelectItem(T newItem);
         public bool ItemIsSelectet(T item) => _selectedItems.Contains(item.Date);
         public bool DateIsSelected(DateTime dateTime) => _selectedItems.Contains(dateTime);
@@ -27,14 +27,24 @@ namespace ProjectShedule.Shedule.Calendar.Controls.SelectionEngine
     }
     internal class SingleSelectionDayEngine : BaseSelectionHasDateEngine<DayModel>
     {
+        private DayModel _selectedDayModel;
         public override void SelectItem(DayModel newDay)
         {
             newDay.IsSelected = !newDay.IsSelected;
 
             if (_selectedItems.Contains(newDay.Date))
+            {
+                _selectedDayModel = null;
                 _selectedItems.Clear();
+            }
             else
-                _selectedItems.Insert(0, newDay.Date);
+            {
+                if (_selectedDayModel != null)
+                    _selectedDayModel.IsSelected = !_selectedDayModel.IsSelected;
+                _selectedDayModel = newDay;
+                _selectedItems.Replace(_selectedDayModel.Date);
+            }
+                
         }
     }
 }
