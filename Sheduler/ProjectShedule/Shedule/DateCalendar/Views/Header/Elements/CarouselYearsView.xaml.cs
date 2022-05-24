67ba032b46b805ce0967058e205b9bcd26ce215d
Insaf <89502937+ClioBro@users.noreply.github.com>
2022-05-24 06:28:00 +0300

    Update CarouselYearsView.xaml.cs
    Removing unwanted properties

--- Sheduler/ProjectShedule/Shedule/DateCalendar/Views/Header/Elements/CarouselYearsView.xaml.cs
+++ Sheduler/ProjectShedule/Shedule/DateCalendar/Views/Header/Elements/CarouselYearsView.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using ProjectShedule.Shedule.Calendar.Models;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,13 +14,12 @@ namespace ProjectShedule.Shedule.Calendar.Views.Header
         public bool IsDragging => carouselYear.IsDragging;
 
         public static readonly BindableProperty YearsProperty =
-          BindableProperty.Create(nameof(Years), typeof(List<YearModel>), typeof(CarouselYearsView), new List<YearModel>(), BindingMode.TwoWay);
-        public List<YearModel> Years
+          BindableProperty.Create(nameof(Years), typeof(ObservableCollection<YearModel>), typeof(CarouselYearsView), new ObservableCollection<YearModel>(), BindingMode.TwoWay);
+        public ObservableCollection<YearModel> Years
         {
-            get => (List<YearModel>)GetValue(YearsProperty);
+            get => (ObservableCollection<YearModel>)GetValue(YearsProperty);
             set => SetValue(YearsProperty, value);
         }
-
 
         public static readonly BindableProperty DisplayedYearProperty =
           BindableProperty.Create(nameof(DisplayedYear), typeof(YearModel), typeof(CarouselYearsView), new YearModel(), BindingMode.TwoWay);
@@ -28,35 +27,6 @@ namespace ProjectShedule.Shedule.Calendar.Views.Header
         {
             get => (YearModel)GetValue(DisplayedYearProperty);
             set => SetValue(DisplayedYearProperty, value);
-        }
-
-        public static readonly BindableProperty MaximumYearProperty =
-          BindableProperty.Create(nameof(MaximumYear), typeof(int), typeof(CarouselYearsView), 2040, BindingMode.OneWay);
-        
-        public int MaximumYear
-        {
-            get => (int)GetValue(MaximumYearProperty);
-            set
-            {
-                if (value >= DateTime.MinValue.Year && value <= DateTime.MaxValue.Year)
-                {
-                    SetValue(MaximumYearProperty, value);
-                }
-            }
-        }
-
-        public static readonly BindableProperty MinimumYearProperty =
-          BindableProperty.Create(nameof(MinimumYear), typeof(int), typeof(CarouselYearsView), 2020, BindingMode.OneWay);
-        public int MinimumYear
-        {
-            get => (int)GetValue(MinimumYearProperty);
-            set
-            {
-                if (value >= DateTime.MinValue.Year && value <= DateTime.MaxValue.Year)
-                {
-                    SetValue(MinimumYearProperty, value);
-                }
-            }
         }
         #endregion
         public CarouselYearsView()
