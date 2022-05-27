709269b2b4bfbbf7a66001395591b777172dada7
Insaf <89502937+ClioBro@users.noreply.github.com>
2022-05-27 21:23:26 +0300

    Create INotifyThemeChanger.cs

---
+++ Sheduler/ProjectShedule/GlobalSetting/Settings/AppTheme/INotifyThemeChanger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ProjectShedule.GlobalSetting.Settings.AppTheme
+{
+    public interface INotifyThemeChanger
+    {
+        event EventHandler<ThemeChangedEventArgs> ThemeChanged;
+    }
+}
